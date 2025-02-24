dbb57d5aff0f820eb4982836315c72cab4e82993
etaoxing <etaoxing@users.noreply.github.com>
2025-02-24 12:42:55 -0500

    Include envs and examples in dflex setup

--- setup.py
+++ setup.py
@@ -5,9 +5,13 @@
 # distribution of this software and related documentation without an express
 # license agreement from NVIDIA CORPORATION is strictly prohibited.
 
-import setuptools
+from setuptools import find_packages, setup
 
-setuptools.setup(
+packages = find_packages(where="dflex")
+packages += find_packages(exclude=["envs*", "examples*"]) + ["dflex.envs", "dflex.examples"]
+package_dir = {"": "dflex", "dflex.envs": "envs", "dflex.examples": "examples"}
+
+setup(
     name="dflex",
     version="0.0.1",
     author="NVIDIA",
@@ -15,7 +19,10 @@ setuptools.setup(
     description="Differentiable Multiphysics for Python",
     long_description="",
     long_description_content_type="text/markdown",
-    packages=setuptools.find_packages(),
+    packages=packages,
+    package_dir=package_dir,
+    include_package_data=True,
+    zip_safe=False,
     classifiers=[
         "Programming Language :: Python :: 3",
         "Operating System :: OS Independent",
